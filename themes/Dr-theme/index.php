37b21d7c678f7c5de1840a8a2756898d395de425
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-09-11 11:03:25 +0330

    start personal page

---
+++ themes/Dr-theme/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html lang="fa">
+<head>
+	<title></title>
+
+	<meta name="viewport" content="width=device-width, maximum-scale=1.0, initial-scale=1.0, user-scalable=no">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=IE8">
+	<meta charset="UTF-8">
+	<meta name="robots" content="INDEX,FOLLOW">
+	<meta name="description" content="dr">
+	<meta name="keywords" content="dr,cms,mediateq">
+	<meta http-equiv="Content-Language" content="Fa">
+	<meta http-equiv="Designer" content="مدیاتک">
+	<meta name="Generator" content="مدیاتک">
+	<meta name="Author" content="مدیاتک">
+
+	<link rel="icon" href="favicon.ico" type="image/x-icon" />
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+
+	<link rel="stylesheet" type="text/css" href="style.css" media="all" />
+</head>
+<body>
+	
+</body>
+</html>
