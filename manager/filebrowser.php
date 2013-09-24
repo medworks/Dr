eca5bbf19244fc45fa71a83c4ca530330f30c98e
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-09-24 09:24:31 +0330

    fix file upload

--- manager/filebrowser.php
+++ manager/filebrowser.php
@@ -11,22 +11,7 @@
 		die(); // solve a security bug
 	}
     $pics = "";
-	$dir = "";	
-	if ($_GET['item']=="newsmgr")
-	{	   
-		$dir='../newspics';
-	}	
-	else
-	if ($_GET['item']=="worksmgr")
-	{	 
-		$dir='../workspics';
-	}
-	else
-	if ($_GET['item']=="slidesmgr")
-	{	 
-		$dir='../slidespics';
-	}
-	else
+	$dir = "";
 	if ($_GET['item']=="gallerymgr")
 	{	 
 		$dir='../gallerypics';
@@ -39,25 +24,7 @@
    
 $html=<<<cd
 <script type='text/javascript'>
-	$(document).ready(function(){		  	 
-		$("#tab1").click(function(){	
-			$.get('ajaxcommand.php?cmd=file&item=newspics', function(data) {
-						$('#catab1 ul').html(data);
-				});			
-			return false;
-		});		
-		$("#tab2").click(function(){
-		$.get('ajaxcommand.php?cmd=file&item=workspics', function(data) {
-						$('#catab2 ul').html(data);
-				});			
-			return false;
-		});
-		$("#tab3").click(function(){
-		$.get('ajaxcommand.php?cmd=file&item=slidespics', function(data) {
-						$('#catab3 ul').html(data);
-				});			
-			return false;
-		});
+	$(document).ready(function(){
 		$("#tab4").click(function(){
 		$.get('ajaxcommand.php?cmd=file&item=gallerypics', function(data) {
 						$('#catab4 ul').html(data);
@@ -70,7 +37,7 @@ $html=<<<cd
 				});			
 			return false;
 		});
-		$("#tab1").click();
+		$("#tab4").click();
 	});
 </script>	   
 	<div class="picmanager">
@@ -90,30 +57,9 @@ $html=<<<cd
 			<div class="pics cat-box-content cat-box tab" id="cats-tabs-box">
 				<div class="cat-tabs-header">
 					<ul>
-						<li id="tab1"><a href="#catab1">پوشه اخبار</a></li>
-						<li id="tab2"><a href="#catab2">پوشه کارها</a></li>
-						<li id="tab3"><a href="#catab3">پوشه اسلاید ها</a></li>
 						<li id="tab4"><a href="#catab4">پوشه گالری</a></li>
 						<li id="tab5"><a href="#catab5">پوشه کاربران</a></li>
 					</ul>
-				</div>
-				<div class="cat-tabs-wrap" id="catab1">
-					<ul>
-						
-					</ul>
-					<div class="badboy"></div>
-				</div>
-				<div class="cat-tabs-wrap" id="catab2">
-					<ul>
-					
-					</ul>
-					<div class="badboy"></div>
-				</div>
-				<div class="cat-tabs-wrap" id="catab3">
-					<ul>
-						
-					</ul>
-					<div class="badboy"></div>
 				</div>
 				<div class="cat-tabs-wrap" id="catab4">
 					<ul>
